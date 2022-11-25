c030ee2b27194c534c0e9a016483a0cc08334428
Pavel Spichakov <spv-pavel@yandex.ru>
2022-11-25 20:31:28 +0300

    in ship_place add randon, check

--- main.py
+++ main.py
@@ -1,52 +1,69 @@
 from myclass import Ship
+from random import randrange
 
 
-def print_data(data_):
+def field_print(_field):
     print(' ', end=' ')
-    for i_ in range(1, len(data_[0]) + 1):
+    for i_ in range(1, len(_field[0]) + 1):
         print(i_, end=' ')
     print('\n', end='')
-    for ny, y in enumerate(range(len(data_))):
+    for ny, y in enumerate(range(len(_field))):
         print(ny + 1, end=' ')
-        for x in range(len(data_[y])):
-            print(data_[y][x], end=' ')
+        for x in range(len(_field[y])):
+            print(_field[y][x], end=' ')
         print('\n', end='')
     return ''
 
 
-def place_ship(_ships, _field):
-    for ship in _ships:
-        for i in range(ship.size):
-            _field[ship.y][ship.x + i] = '='
-    return _field
+def ship_place(_ship, _field):
+    # random y, x
+    _ship.y = randrange(6)
+    _ship.x = randrange(6 - _ship.size + 1)
+    if _ship.x < 0:
+        _ship.x = 0
+    # check ship
+    for i in range(_ship.size):
+        if _field[_ship.y][_ship.x + i] != 'O':
+            return False
+    # place ship
+    
+    for i in range(_ship.size):
+        _field[_ship.y][_ship.x + i] = _ship.name
+    return True
 
 
 # army player
-p_ship3_1 = Ship(0, 0, 3)  # three-deck ship
-p_ship2_1 = Ship(1, 0, 2)  # two-deck ship
-p_ship2_2 = Ship(2, 0, 2)  # two-deck ship
-p_ship1_1 = Ship(3, 0, 1)  # one-deck ship
-p_ship1_2 = Ship(4, 0, 1)  # one-deck ship
-p_ship1_3 = Ship(5, 0, 1)  # one-deck ship
-p_ship1_4 = Ship(5, 3, 1)  # one-deck ship
-ships_player = [p_ship3_1, p_ship2_1, p_ship2_2, p_ship1_1, p_ship2_2, p_ship1_3, p_ship1_4]
+p_ship3_1 = Ship(0, 1, 3, 1)  # three-deck ship
+p_ship2_1 = Ship(1, 1, 2, 2)  # two-deck ship
+p_ship2_2 = Ship(2, 1, 2, 3)  # two-deck ship
+p_ship1_1 = Ship(3, 1, 1, 4)  # one-deck ship
+p_ship1_2 = Ship(4, 1, 1, 5)  # one-deck ship
+p_ship1_3 = Ship(5, 1, 1, 6)  # one-deck ship
+p_ship1_4 = Ship(5, 3, 1, 7)  # one-deck ship
+ships_player = [p_ship3_1, p_ship2_1, p_ship2_2, p_ship1_1, p_ship1_2, p_ship1_3, p_ship1_4]
 # army computer
-c_ship3_1 = Ship(0, 0, 3)  # three-deck ship
-c_ship2_1 = Ship(1, 0, 2)  # two-deck ship
-c_ship2_2 = Ship(2, 0, 2)  # two-deck ship
-c_ship1_1 = Ship(3, 0, 1)  # one-deck ship
-c_ship1_2 = Ship(4, 0, 1)  # one-deck ship
-c_ship1_3 = Ship(5, 0, 1)  # one-deck ship
-c_ship1_4 = Ship(5, 3, 1)  # one-deck ship
-ships_computer = [c_ship3_1, c_ship2_1, c_ship2_2, c_ship1_1, c_ship2_2, c_ship1_3, c_ship1_4]
+c_ship3_1 = Ship(0, 0, 3, 1)  # three-deck ship
+c_ship2_1 = Ship(1, 0, 2, 2)  # two-deck ship
+c_ship2_2 = Ship(2, 0, 2, 3)  # two-deck ship
+c_ship1_1 = Ship(3, 0, 1, 4)  # one-deck ship
+c_ship1_2 = Ship(4, 0, 1, 5)  # one-deck ship
+c_ship1_3 = Ship(5, 0, 1, 6)  # one-deck ship
+c_ship1_4 = Ship(5, 2, 1, 7)  # one-deck ship
+ships_computer = [c_ship3_1, c_ship2_1, c_ship2_2, c_ship1_1, c_ship1_2, c_ship1_3, c_ship1_4]
 
 field_player = [['O' for x in range(6)] for y in range(6)]
-field_computer = [['O' for x in range(6)] for y in range(6)]
+field_computer = [['O' for x1 in range(6)] for y1 in range(6)]
 
-field_player = place_ship(ships_player, field_player)
-# print(list(enumerate(data)))
+for ship in ships_player:
+    print(ship, end='')
+print()
+
+for ship in ships_player:
+    ship_place(ship, field_player)
 # print('computer:')
-# print(print_data(field_computer))
-print('player:')
-print(print_data(field_player), end='')
+print(field_print(field_player))
 
+# print('player:')
+# print(fild_print(field_player), end='')
+for ship in ships_player:
+    print(ship, end='')
--- myclass.py
+++ myclass.py
@@ -1,6 +1,9 @@
 class Ship:
-    def __init__(self, y, x, size):
+    def __init__(self, y, x, size, name):
         self.y = y
         self.x = x
         self.size = size
+        self.name = name
 
+    def __str__(self):
+        return f'{self.y, self.x, self.size}'
