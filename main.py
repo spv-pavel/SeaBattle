e9771246b3bb3ade0e34611b4c2d3601d8a561e8
Pavel Spichakov <spv-pavel@yandex.ru>
2022-11-28 18:23:34 +0300

    field_clear and ships_place_rnd

--- main.py
+++ main.py
@@ -1,50 +1,68 @@
 from myclass import Ship
 from random import randrange
-# import time
+import time
 
 
-def field_print(_field):
+def field_print(field_):
+    print()
+    # print(field_.__str__)
     print(' ', end=' ')
-    for i_ in range(0, len(_field[0])):  # range(1, len(_field[0]) + 1)
+    for i_ in range(0, len(field_[0])):  # range(1, len(_field[0]) + 1)
         print(i_, end=' ')
     print('\n', end='')
-    for ny, y in enumerate(range(len(_field))):
-        print(ny, end=' ')  # ny + 1
-        for x in range(len(_field[y])):
-            print(_field[y][x], end=' ')
+    for ny_, y_ in enumerate(range(len(field_))):
+        print(ny_, end=' ')  # ny_ + 1
+        for x_ in range(len(field_[y_])):
+            print(field_[y_][x_], end=' ')
         print('\n', end='')
     return ''
 
 
-def ship_place_rnd(_ship, _field):
-    check = 0
-    while check == 0:
-        check = 1
-        _ship.y = randrange(6)
-        y_start_check = _ship.y - 1
-        if y_start_check < 0:
-            y_start_check = 0
-        y_end_check = _ship.y + 1
-        if y_end_check > 5:
-            y_end_check = 5
-        _ship.x = randrange(6)
-        if _ship.x + _ship.size > 6:
-            _ship.x = _ship.x - (_ship.x + _ship.size - 6)
-        x_start_check = _ship.x - 1
-        if x_start_check < 0:
-            x_start_check = 0
-        x_end_check = _ship.x + _ship.size
-        if x_end_check > 5:
-            x_end_check = 5
-        for y in range(y_start_check, y_end_check + 1):
-            for x in range(x_start_check, x_end_check + 1):
-                if _field[y][x] != '0':  # and _field[y][x] != '*':
-                    check = 0
-                # else:
-                #    _field[y][x] = '*'
-        if check == 1:
-            for i in range(_ship.size):
-                _field[_ship.y][_ship.x + i] = _ship.name
+def field_clear(field_):
+    for y_ in range(len(field_)):
+        for x_ in range(len(field_[y_])):
+            field_[y_][x_] = '0'
+    return ''
+
+
+def ships_place_rnd(ships_, field_):
+    check1_ = 0
+    while check1_ == 0:
+        check1_ = 1
+        for ship_ in ships_:
+            check2_, n_ = 0, 0
+            while check2_ == 0:
+                n_ += 1
+                check2_ = 1
+                ship_.y = randrange(6)
+                y_start_check = ship_.y - 1
+                if y_start_check < 0:
+                    y_start_check = 0
+                y_end_check = ship_.y + 1
+                if y_end_check > 5:
+                    y_end_check = 5
+                ship_.x = randrange(6)
+                if ship_.x + ship_.size > 6:
+                    ship_.x = ship_.x - (ship_.x + ship_.size - 6)
+                x_start_check = ship_.x - 1
+                if x_start_check < 0:
+                    x_start_check = 0
+                x_end_check = ship_.x + ship_.size
+                if x_end_check > 5:
+                    x_end_check = 5
+                for y in range(y_start_check, y_end_check + 1):
+                    for x in range(x_start_check, x_end_check + 1):
+                        if field_[y][x] != '0':  # and field_[y][x] != '*':
+                            check2_ = 0
+                        # else:
+                        #    field_[y][x] = '*'
+                if check2_ == 1:
+                    for i in range(ship_.size):
+                        field_[ship_.y][ship_.x + i] = ship_.name
+                if n_ > 100:
+                    check1_ = 0
+                    field_clear(field_)
+                    break
     return True
 
 
@@ -70,12 +88,5 @@ ships_computer = [c_ship3_1, c_ship2_1, c_ship2_2, c_ship1_1, c_ship1_2, c_ship1
 field_player = [['0' for x in range(6)] for y in range(6)]
 field_computer = [['0' for x1 in range(6)] for y1 in range(6)]
 
-for ship in ships_player:
-    ship_place_rnd(ship, field_player)
-# ship_place_rnd(p_ship2_1, field_player)
-# ship_place(p_ship2_1, field_player)
-# print('computer:')
-print(field_print(field_player))
-print(p_ship2_1)
-# print('player:')
-# print(fild_print(field_player), end='')
+ships_place_rnd(ships_player, field_player)
+print(field_print(field_player), end='')
