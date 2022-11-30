6ab8cecb2c74a83fa2cb743bb68043527931f5e9
Pavel Spichakov <spv-pavel@yandex.ru>
2022-11-30 17:17:29 +0300

    class Ship: minus hits, condition

--- myclass.py
+++ myclass.py
@@ -1,10 +1,8 @@
 class Ship:
-    def __init__(self, y, x, size, hits=0, condition='whole'):
+    def __init__(self, y, x, size):
         self.y = y
         self.x = x
         self.size = size
-        self.hits = hits
-        self.condition = condition
 
     def __str__(self):
-        return f'{self.y, self.x, self.size, self.hits, self.condition}'
+        return f'{self.y, self.x, self.size}'
